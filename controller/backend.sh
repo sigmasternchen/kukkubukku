101dd0bcc692599b1551dd572096bee11742b646
overflowerror <mail@overflowerror.com>
2022-03-30 22:07:35 +0200

    adding recipes now works

--- controller/backend.sh
+++ controller/backend.sh
@@ -10,3 +10,36 @@ backendHome() {
 	content="$(getRecipesByUsername "$username" | template "templates/backend.fragment.templ")"
 	template "templates/layout.html.templ"
 }
+
+route GET "/backend/new" backendNew
+backendNew() {
+	requireLoggedIn
+	htmlContent
+	endHeaders
+
+	title="Backend - New"
+	content="$(template "templates/backend.new.fragment.templ")"
+	template "templates/layout.html.templ"
+}
+
+route POST "/backend/new" backendAdd
+backendAdd() {
+	requireLoggedIn
+	cacheFormData
+
+	name="$(formData "name")"
+	description="$(formData "description")"
+	qts="$(formData "qts")"
+	ingredients="$(formData "ingredients")"
+
+	if test "$(echo "$qts" | wc -l)" -ne "$(echo "$ingredients" | wc -l)"; then
+		status 400
+		endHeaders
+		return
+	fi
+
+	addRecipe "$name" "$description" "$username" "$qts" "$ingredients"
+
+	redirect "/backend"
+	endHeaders
+}
--- data/recipes.sh
+++ data/recipes.sh
@@ -14,3 +14,40 @@ getRecipesByUsername() {
 			users.username = '$(escape "$username")'
 EOF
 }
+
+addRecipe() {
+	local name="$1"
+	local description="$2"
+	local username="$3"
+	local qts="$4"
+	local ingredients="$5"
+
+	local nrIngredients="$(echo "$qts" | wc -l)"
+
+	template <(cat <<EOF
+		START TRANSACTION;
+
+		SELECT
+			@uid := id
+		FROM users
+		WHERE username = '$(escape "$username")';
+
+		INSERT INTO recipes
+			(userFk, name, description) VALUES
+			(@uid, '$(escape "$name")', '$(escape "$description")');
+
+		SELECT @rid := LAST_INSERT_ID();
+
+		{{ for i in \$(seq $nrIngredients); do }}
+			INSERT INTO ingredients
+				(recipeFk, name, quantity) VALUES
+				(@rid,
+					'{{ escape "\$(echo "\$ingredients" | tail -n +\$i | head -n 1)" | tr -d \\\\n }}',
+					'{{ escape "\$(echo "\$qts" | tail -n +\$i | head -n 1)" | tr -d \\\\n }}'
+				);
+		{{ done }}
+
+		COMMIT;
+EOF
+	) | execute
+}
